c50ed01259ecc2e8817339373d17f69545ec5ef6
Yoshiki-Hidaka <y.hidaka.kobe@gmail.com>
2020-10-23 18:35:36 +0900

    大まかなレイアウトをBoostrapで整えた ユーザー登録画面の作成 ルーティングのリファクタリング

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,9 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
+  
+  # test環境下でもapplication_helperを使えるように追加
+  include ApplicationHelper
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
