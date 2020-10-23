1c77f9f2e26d4fd334383a15edeadf7d8b16cdac
Yoshiki-Hidaka <y.hidaka.kobe@gmail.com>
2020-10-23 10:03:31 +0900

    finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
+  validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
+
+  has_many :microposts
 end
